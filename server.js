0c04cc64dce28345f7e5e4f20832b0a754c82f51
Nii <niiashikwei@hotmail.com>
2015-06-29 07:46:35 +0000

    Nii - now serving index file instead of string

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Welcome to Disruptive Ideaz!');
+  res.sendfile('./public/index.html');
 });
 
 var server = app.listen(process.env.PORT || 5000, function () {
