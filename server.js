9ce4d6ae720126584ba736ab874ad7b93fa32d62
Nii <niiashikwei@hotmail.com>
2015-07-07 13:30:32 +0000

    Nii - added a few links to app projects, some css as well

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ app.get('/', function (req, res) {
   res.sendFile('./public/index.html', {"root": __dirname});
 });
 
+app.get('/tab_viewer', function (req, res) {
+    res.sendFile('./public/app_pages/tab_viewer.html', {"root": __dirname});
+});
+
+app.get('/dropbox_cms', function (req, res) {
+    res.sendFile('./public/app_pages/dropbox_cms.html', {"root": __dirname});
+});
+
+app.use('/css', express.static(__dirname + '/public/css'));
+
 var server = app.listen(process.env.PORT || 5000, function () {
 
   var host = server.address().address;
